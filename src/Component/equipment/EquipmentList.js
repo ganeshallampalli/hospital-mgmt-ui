c84531642f5fca66a4868724d32db72bfb85ff91
Ganesh Allampalli <ganeshallampalli@gmail.com>
2021-06-14 12:28:38 +0530

    :art: refactor: formatting the code

--- src/Component/equipment/EquipmentList.js
+++ src/Component/equipment/EquipmentList.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import React, { useEffect } from "react";
-import {Button, Card, CardDeck, Col, Form} from "react-bootstrap";
-import { useHistory } from "react-router-dom";
-import { connect } from "react-redux";
+import React, {useEffect} from "react";
+import {Card, CardDeck, Form} from "react-bootstrap";
+import {useHistory} from "react-router-dom";
+import {connect} from "react-redux";
 
 const EquipmentList = (props) => {
   const [result, setResult] = React.useState({});
@@ -14,16 +14,16 @@ const EquipmentList = (props) => {
 
   const getEquipmentsList = (pageNo = 0) => {
     axios
-      .get("/api/v1/equipment/all", { params: { pageNo } })
-      .then((response) => {
-        setPageNo(pageNo);
-        setResult(response.data);
-        setEquipments(response.data); // TODO: Use this when the API is working
-      })
-      .catch((response) => {
-        console.log(response);
-      });
-   // let equipments = JSON.parse(localStorage.getItem("equipments")); // TODO: Remove this. Only for my testing
+        .get("/api/v1/equipment/all", {params: {pageNo}})
+        .then((response) => {
+          setPageNo(pageNo);
+          setResult(response.data);
+          setEquipments(response.data); // TODO: Use this when the API is working
+        })
+        .catch((response) => {
+          console.log(response);
+        });
+    // let equipments = JSON.parse(localStorage.getItem("equipments")); // TODO: Remove this. Only for my testing
     //setEquipments(equipments); // TODO: Remove this. Only for my testing
   };
   useEffect(() => {
@@ -32,13 +32,13 @@ const EquipmentList = (props) => {
 
   const deleteEquipment = (id) => {
     axios
-      .delete("/api/v1/equipment/" + id)
-      .then((response) => {
-        getEquipmentsList();
-      })
-      .catch((reponse) => {
-        console.log(reponse);
-      });
+        .delete("/api/v1/equipment/" + id)
+        .then((response) => {
+          getEquipmentsList();
+        })
+        .catch((reponse) => {
+          console.log(reponse);
+        });
   };
   const newEquipment = () => {
     history.push("/new-equipment");
@@ -56,42 +56,44 @@ const EquipmentList = (props) => {
     const emailId = emailIdRef.current.value;
     const firstName = firstNameRef.current.value;
     axios
-      .get("/api/v1/user/search", {
-        params: { role: "ROLE_ADMIN", emailId, firstName, pageNo },
-      })
-      .then((response) => {
-        setPageNo(0);
-        setResult(response.data);
-      })
-      .catch((reponse) => {
-        console.log(reponse);
-      });
+        .get("/api/v1/user/search", {
+          params: {role: "ROLE_ADMIN", emailId, firstName, pageNo},
+        })
+        .then((response) => {
+          setPageNo(0);
+          setResult(response.data);
+        })
+        .catch((reponse) => {
+          console.log(reponse);
+        });
   };
   let editAllowed = false;
   if (props?.user?.role === "ROLE_ADMIN") {
     editAllowed = true;
   }
   return (
-    <div>
-      <Form.Row>
-        <h3>Equipment List</h3>
-      </Form.Row>
-      <CardDeck style={{gap: '20px'}}>
-        {equipments.map(equipment => {
-          return (<Card style={{height: '350px', minWidth: '300px', maxWidth: "300px"}}>
-            <Card.Img style={{height: '300px', width: '300px'}} variant="top" src={equipment.image} />
-            <Card.Body>
-              <Card.Title>{equipment.name}</Card.Title>
-            </Card.Body>
-          </Card>);
-        })}
-      </CardDeck>
-    </div>
+      <div>
+        <Form.Row>
+          <h3>Equipment List</h3>
+        </Form.Row>
+        <CardDeck style={{gap: '20px'}}>
+          {equipments.map(equipment => {
+            return (<Card style={{height: '350px', minWidth: '300px', maxWidth: "300px"}}>
+              <Card.Img style={{height: '300px', width: '300px'}} variant="top" src={equipment.image}/>
+              <Card.Body>
+                <Card.Title>{equipment.name}</Card.Title>
+              </Card.Body>
+            </Card>);
+          })}
+        </CardDeck>
+      </div>
   );
 };
+
 function mapStateToProps(state) {
   return {
     user: state.userData.user,
   };
 }
+
 export const EquipmentConnectedList = connect(mapStateToProps, null)(EquipmentList);
